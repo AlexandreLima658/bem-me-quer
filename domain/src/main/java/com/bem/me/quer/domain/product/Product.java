83def2063e53b38638b4aa4516f93884838efb7c
Alexandre <= alexandrec@alu.ufc.br>
2025-09-15 10:55:13 -0300

    feat: create a product entity

--- domain/src/main/java/com/bem/me/quer/domain/product/Product.java
+++ domain/src/main/java/com/bem/me/quer/domain/product/Product.java
@@ -1,4 +1,34 @@
 package com.bem.me.quer.domain.product;
 
-public class Product {
+import com.bem.me.quer.domain.category.attributes.CategoryId;
+import com.bem.me.quer.domain.commons.entities.AggregateRoot;
+import com.bem.me.quer.domain.product.attributes.ProductId;
+
+public class Product extends AggregateRoot<ProductId> {
+
+    private String name;
+    private String description;
+    private CategoryId categoryId;
+
+    Product(
+            final ProductId productId,
+            final String name,
+            final String description,
+            final CategoryId categoryId
+    ) {
+        super(productId);
+        this.name = name;
+        this.description = description;
+        this.categoryId = categoryId;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String description() {
+        return description;
+    }
+
+
 }
