43312ee1390f7207da1eebc0f125558017dd4604
Alexandre <= alexandrec@alu.ufc.br>
2025-09-23 16:33:31 -0300

    feat: create a customer entity

---
+++ domain/src/main/java/com/bem/me/quer/domain/customer/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.bem.me.quer.domain.customer;
+
+import com.bem.me.quer.domain.customer.attributes.CustomerId;
+import com.bem.me.quer.domain.Repository;
+
+public interface CustomerRepository extends Repository<Customer, CustomerId> {
+
+}
